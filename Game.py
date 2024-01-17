508e57eb0a7cb2747fa24be2b9fc07b802f5793b
18Abhinav07 <abhinavpangaria2003@gmail.com>
2024-01-17 11:53:00 +0530

    Initial work just the board
    completed the game.
    yet not the most efficient.

--- Game.py
+++ Game.py
@@ -1,48 +1,73 @@
-import sys
+import time
+
 import pygame
-from Values import *
-from Mechanics import *
+from pygame.locals import *
 from Puzzle_AI import *
 
+# Initialize Pygame
+pygame.init()
 
-class GAME:
-    def __init__(self):
-        self.playing = None
-        pygame.init()
-        self.SCREEN = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-        pygame.display.set_caption("8 Puzzle Game")
-        self.SCREEN.fill(BLACK)
+# Set up some constants
+WIDTH, HEIGHT = 400, 400
+ROWS, COLS = 3, 3
+SQUARE_SIZE = WIDTH // (COLS + 2)  # Leave space for margins
+MARGIN = SQUARE_SIZE  # Margin size
 
-    def draw(self):
-        self.drawGrid()
+# Set up the display
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 
-    def drawGrid(self):
-        # Draw the grid
-        for x in range(WINDOW_WIDTH // 4, WINDOW_WIDTH // 4 + blockSize * 3, blockSize):
-            for y in range(WINDOW_WIDTH // 8, blockSize * 3, blockSize):
-                rect = pygame.Rect(x, y, blockSize, blockSize)
-                pygame.draw.rect(self.SCREEN, WHITE, rect, 1)
+# Set up the puzzle state
+start_state = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
 
-    def newGame(self):
-        pass
 
-    def run(self):
-        self.playing = True
-        while self.playing:
-            self.draw()
-            self.events()
+def draw_window(state, message):
+    WIN.fill((255, 255, 255))
 
-    def events(self):
+    # Draw the grid
+    for i in range(ROWS):
+        for j in range(COLS):
+            pygame.draw.rect(WIN, (0, 0, 0),
+                             (MARGIN + j * SQUARE_SIZE, MARGIN + i * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE), 1)
+
+            # Draw the numbers
+            font = pygame.font.Font(None, 36)
+            text = font.render(str(state[i][j]), 1, (0, 0, 0))
+            WIN.blit(text, (MARGIN + j * SQUARE_SIZE + SQUARE_SIZE // 2 - text.get_width() // 2,
+                            MARGIN + i * SQUARE_SIZE + SQUARE_SIZE // 2 - text.get_height() // 2))
+
+    # Draw the message
+    font = pygame.font.Font(None, 24)
+    text = font.render(message, 1, (0, 0, 0))
+    WIN.blit(text, (WIDTH // 2 - text.get_width() // 2, HEIGHT - MARGIN // 2 - text.get_height() // 2))
+
+    pygame.display.update()
+
+
+def main():
+    clock = pygame.time.Clock()
+    run = True
+    message = "Welcome to 8 Puzzle!"
+
+    while run:
+        clock.tick(60)
+        solver = PUZZLE_SOlVER(start_state)
+        draw_window(start_state, "The solver has found a way")
+
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
+            if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
+                run = False
 
-        pygame.display.update()
+        solution_set = solver.get_solution()
+
+        for state in solution_set:
+            time.sleep(0.05)
+            draw_window(state, "Working it out")
+
+        draw_window(solver.GOAL_STATE, f"Done. The puzzle has been solved in {solver.count} steps.")
+        time.sleep(2.0)
+        run = False
+    pygame.quit()
 
 
 if __name__ == "__main__":
-    game = GAME()
-    while True:
-        game.newGame()
-        game.run()
+    main()
---
+++ Puzzle_AI.py
@@ -0,0 +1,147 @@
+import time
+import numpy as np
+
+
+class PUZZLE_SOlVER:
+    # [0, 1, 2], [3, 4, 5], [6, 7, 8]
+    # [1, 2, 3], [4, 5, 6], [7, 8, 0]
+
+    # easy check1 [1, 2, 3], [0, 4, 6], [7, 5, 8]
+    # hard check2 [0, 1, 2], [3, 4, 5], [6, 7, 8]
+    def __init__(self, start_state):
+        self.GOAL_STATE = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 0]])
+        self.count = 0
+        self.start_time = time.time()
+        self.start_state = np.asarray(start_state)
+        self.explored_state = []
+        self.unsolvable = []
+        self.solution_set = []
+        if self.isSolvable(self.start_state):
+            if not self.start_analysis(self.start_state):
+                print("No solution found")
+        else:
+            print("Start state is not solvable")
+
+    def goal_test(self, test_state):
+        # check matching the goal state
+        if np.array_equal(self.GOAL_STATE, test_state):
+            return True
+        return False
+
+    def check(self, state):
+        for explored in self.explored_state:
+            if np.array_equal(explored, state):
+                return True
+        for s in self.unsolvable:
+            if np.array_equal(s, state):
+                return True
+        return False
+
+    def find_legal_actions(self, ind_blank):
+        legal_action = ['U', 'D', 'L', 'R']
+        i = ind_blank[0][0]
+        j = ind_blank[1][0]
+        if i == 0:  # up is disabled
+            legal_action.remove('U')
+        elif i == 2:  # down is disabled
+            legal_action.remove('D')
+        if j == 0:  # left is disabled
+            legal_action.remove('L')
+        elif j == 2:  # right is disabled
+            legal_action.remove('R')
+        return legal_action
+
+    def getInvCount(self, arr):
+        inv_count = 0
+        empty_value = 0
+        for i in range(0, 9):
+            for j in range(i + 1, 9):
+                if arr[j] != empty_value and arr[i] != empty_value and arr[i] > arr[j]:
+                    inv_count += 1
+        return inv_count
+
+    def isSolvable(self, state):
+        # Count inversions in given 8 puzzle
+        inv_count = self.getInvCount([j for sub in state for j in sub])
+
+        # return true if inversion count is even.
+        return inv_count % 2 == 0
+
+    def successor_function(self, state):
+        # take the state , find the actions , in increasing order of the
+        # number of misplaced tiles return all the successor states.
+        self.explored_state.append(state)
+        ind_blank = np.where(state == 0)
+        legal_actions = self.find_legal_actions(ind_blank)
+        successor_states = []
+        h_mis_tiles = []
+        for action in legal_actions:
+            successor_state, mis_tiles = self.transition_function(state, action)
+            successor_states.append(successor_state)
+            h_mis_tiles.append(mis_tiles)
+        values = list(zip(h_mis_tiles, successor_states))
+        values = sorted(values, key=lambda x: x[0])
+        successor_states = [value[1] for value in values]
+        # return the states in increasing order of the misplaced tiles.
+        final_successor_states = np.asarray(successor_states)
+        return final_successor_states
+
+    def heuristic(self, state):
+        # Number of misplaced tiles
+        misplaced_tiles = np.sum(state != self.GOAL_STATE) - 1  # subtract 1 for the blank space
+
+        # Manhattan distance
+        manhattan_distance = 0
+        for i in range(1, 9):  # 1-8 for 8-puzzle
+            pos_state = np.where(state == i)
+            pos_goal = np.where(self.GOAL_STATE == i)
+            manhattan_distance += abs(pos_state[0] - pos_goal[0]) + abs(pos_state[1] - pos_goal[1])
+
+        # Return sum of misplaced tiles and Manhattan distance
+        return misplaced_tiles + manhattan_distance
+
+    def transition_function(self, state, action):
+        new_state = state.copy()
+
+        # Find the index of the blank space (0)
+        ind_blank = np.where(state == 0)
+        i, j = ind_blank[0][0], ind_blank[1][0]
+
+        # Swap the blank space with the adjacent tile based on the action
+        if action == 'U':
+            new_state[i, j], new_state[i - 1, j] = new_state[i - 1, j], new_state[i, j]
+        elif action == 'D':
+            new_state[i, j], new_state[i + 1, j] = new_state[i + 1, j], new_state[i, j]
+        elif action == 'L':
+            new_state[i, j], new_state[i, j - 1] = new_state[i, j - 1], new_state[i, j]
+        elif action == 'R':
+            new_state[i, j], new_state[i, j + 1] = new_state[i, j + 1], new_state[i, j]
+
+        # for this new state calculate the no of misplaced tiles.
+        h_tiles = self.heuristic(new_state)
+        return new_state, h_tiles
+
+    def get_solution(self):
+        return self.solution_set
+
+    def start_analysis(self, state):
+
+        if self.goal_test(state):
+            print('The game was successfully solved!')
+            return True
+
+        successor_states = self.successor_function(state)
+        for s in successor_states:
+            if not self.check(s) and self.isSolvable(s):
+                print(f" Checking the state: {s}")
+                self.count += 1
+                self.solution_set.append(s)
+                if self.start_analysis(s):
+                    return True
+                else:
+                    self.solution_set.pop(-1)
+                    self.unsolvable.append(s)
+            else:
+                self.unsolvable.append(s)
+
+        return False
